89f329b34a3c99fbf1e90566797ec4e0b42510b4
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2024-01-30 16:49:11 +0300

    drop unused

--- openjfx/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module kotlin.graphics.imgui.openjfx {
-
-    requires javafx.graphics;
-
-    requires kotlin.stdlib;
-
-    requires kotlin.graphics.imgui.core;
-    requires kotlin.graphics.glm;
-}
--- openjfx/src/main/kotlin/imgui/impl/OpenJFXDummyMain.kt
+++
@@ -1,9 +0,0 @@
-package imgui.impl
-
-import javafx.application.Application
-import javafx.stage.Stage
-import kotlin.system.exitProcess
-
-class OpenJFXDummyMain: Application() {
-    override fun start(primaryStage: Stage?) { exitProcess(0) }
-}
