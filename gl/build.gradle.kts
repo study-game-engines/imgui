0f06d441a1c2ab4e9a999e674a5cfea53ae8e470
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2024-01-30 21:02:10 +0300

    renderdoc - tested not working

--- gl/build.gradle.kts
+++ gl/build.gradle.kts
@@ -25,3 +25,22 @@ tasks {
         compilerOptions.freeCompilerArgs.addAll("-opt-in=kotlin.RequiresOptIn", "-Xallow-kotlin-package")
     }
 }
+
+/*examples*/
+
+sourceSets.create("examples") {
+    java.srcDirs(File("D:\\workspace\\imgui\\gl\\src\\examples\\kotlin"))
+    compileClasspath += sourceSets.main.get().runtimeClasspath + sourceSets.main.get().compileClasspath
+    runtimeClasspath += sourceSets.main.get().runtimeClasspath + sourceSets.main.get().compileClasspath
+}
+
+tasks.create("demoJar", Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "examples.DemoKt"
+    }
+    archiveBaseName.set("demo")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.getByName("examples").runtimeClasspath.filter { file -> file.exists() }.map { if (it.isDirectory()) it else zipTree(it) })
+    from(sourceSets.getByName("examples").output) // https://stackoverflow.com/a/44815444/909169
+    with(tasks.jar.get())
+}
